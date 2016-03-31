23945da074ae8c6edd183832078964dc77f807fc
zaifrun <martinknud@gmail.com>
2016-03-31 15:51:08 +0200

    changed

--- app/build/generated/source/r/debug/com/iducate/actionbardemo/R.java
+++ app/build/generated/source/r/debug/com/iducate/actionbardemo/R.java
@@ -18,10 +18,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int item_about=0x7f080001;
+        public static final int item_about=0x7f080002;
         public static final int item_delete=0x7f080003;
         public static final int item_help=0x7f080000;
-        public static final int item_refresh=0x7f080002;
+        public static final int item_refresh=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
